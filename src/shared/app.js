af489d4a4c3225e78cda90f1358e17175ed3f161
Matthew Epler <matthewepler@gmail.com>
2017-07-04 08:53:27 -0400

    adds server-side rendering and helmet

--- src/shared/app.js
+++ src/shared/app.js
@@ -3,7 +3,9 @@
 import React from 'react'
 import { Switch } from 'react-router'
 import { Route } from 'react-router-dom'
-import { APP_NAME } from '../shared/config'
+import Helmet from 'react-helmet'
+
+import { APP_NAME } from './config'
 import Nav from './components/nav'
 import HomePage from './components/page/home'
 import HelloPage from './components/page/hello'
@@ -13,11 +15,11 @@ import {
   HOME_PAGE_ROUTE,
   HELLO_PAGE_ROUTE,
   HELLO_ASYNC_PAGE_ROUTE
-} from '../shared/routes/routes'
+} from './routes/routes'
 
 const App = () =>
   <div>
-    <h1>{APP_NAME}</h1>
+    <Helmet titleTemplate={`%s | ${APP_NAME}`} defaultTitle={APP_NAME} />
     <Nav />
     <Switch>
       <Route exact path={HOME_PAGE_ROUTE} render={() => <HomePage />} />
